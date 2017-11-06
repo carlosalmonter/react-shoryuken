f354c23465cab43be9709bf6c06cc90caef9f562
carlosalmonter <carlosalmonter@gmail.com>
2017-11-06 17:40:32 -0400

    changing as functional

--- src/components/HOC/withLoading.js
+++ src/components/HOC/withLoading.js
@@ -1,29 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CircularProgress } from 'material-ui';
 import PropTypes from 'prop-types';
 import colors from '../../config/colors';
 import config from '../../config/config';
 
-const withLoading = WrappedComponent => (
-  class WithLoading extends Component {
-    static propTypes = {
-      isLoadingData: PropTypes.bool.isRequired,
-      isLoadingError: PropTypes.bool.isRequired,
-    };
-    componentDidMount() {
-      console.log('hoc');
-    }
+const withLoading = (WrappedComponent) => {
+  const WithLoadingComponent = props => (
+    <div>
+      {props.isLoadingData && <CircularProgress color={colors.BLACK} />}
+      {props.isLoadingError && config.LOADING_ERROR_MESSAGE}
+      {!props.isLoadingData && <WrappedComponent {...props} />}
+    </div>
+  );
 
-    render() {
-      const { isLoadingData, isLoadingError } = this.props;
-      return (
-        <div>
-          {isLoadingData && <CircularProgress color={colors.BLACK} />}
-          {isLoadingError && config.LOADING_ERROR_MESSAGE}
-          {!isLoadingData && <WrappedComponent {...this.props} />}
-        </div>
-      );
-    }
-  });
+  WithLoadingComponent.propTypes = {
+    isLoadingData: PropTypes.bool.isRequired,
+    isLoadingError: PropTypes.bool.isRequired,
+  };
+
+  return WithLoadingComponent;
+};
 
 export default withLoading;
